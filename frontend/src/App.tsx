8069ccd0a15a99244e7d75f1685cbd4138b4301f
Ben Hardy <39031657+DevBenH@users.noreply.github.com>
2023-12-06 15:14:16 +0000

    Added handleSubmit method to retrieve and pass form data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,60 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Todo, { TodoType } from './Todo';
 
 function App() {
   const [todos, setTodos] = useState<TodoType[]>([]);
 
-  // Initially fetch todo
+  // Initially fetch todos
   useEffect(() => {
     const fetchTodos = async () => {
       try {
-        const todos = await fetch('http://localhost:8080/');
-        if (todos.status !== 200) {
+        const response = await fetch('http://localhost:8080/');
+        if (!response.ok) {
           console.log('Error fetching data');
           return;
         }
 
-        setTodos(await todos.json());
+        const data = await response.json();
+
+        // Ensure that data is an array before setting state
+        if (Array.isArray(data)) {
+          setTodos(data);
+        }
       } catch (e) {
         console.log('Could not connect to server. Ensure it is running. ' + e);
       }
-    }
+    };
 
-    fetchTodos()
+    fetchTodos();
   }, []);
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    console.log('FormData:', formData.get("title"));
+    console.log('FormData:', formData.get("description"));
+    try {
+      const response = await fetch('http://localhost:8080/', {
+        method: 'POST',
+        body: formData,
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+
+      if (response.ok) {
+        console.log('Todo added successfully');
+
+        // Optionally, you can fetch the updated todos here
+      } else {
+        console.log('Error adding todo');
+      }
+    } catch (e) {
+      console.log('Could not connect to server. Ensure it is running. ' + e);
+    }
+  };
 
   return (
     <div className="app">
@@ -31,20 +63,16 @@ function App() {
       </header>
 
       <div className="todo-list">
-        {todos.map((todo) =>
-          <Todo
-            key={todo.title + todo.description}
-            title={todo.title}
-            description={todo.description}
-          />
-        )}
+        {todos.map((todo) => (
+          <Todo key={todo.title + todo.description} title={todo.title} description={todo.description} />
+        ))}
       </div>
 
       <h2>Add a Todo</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input placeholder="Title" name="title" autoFocus={true} />
         <input placeholder="Description" name="description" />
-        <button>Add Todo</button>
+        <button type="submit">Add Todo</button>
       </form>
     </div>
   );
