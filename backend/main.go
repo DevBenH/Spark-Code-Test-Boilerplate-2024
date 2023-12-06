7f39914ea04f23adc7072b1c7f538186f9e6be89
Ben Hardy <39031657+DevBenH@users.noreply.github.com>
2023-12-06 15:13:19 +0000

    Added backend functionality to handle POST form request data

--- backend/main.go
+++ backend/main.go
@@ -1,13 +1,57 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type Todo struct {
+	Title       string `json:"title"`
+	Description string `json:"description"`
+}
+
+var todos []Todo
 
 func main() {
-	// Your code here
+	http.HandleFunc("/", ToDoListHandler)
+	http.ListenAndServe(":8080", nil)
 }
 
 func ToDoListHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 
-	// Your code here
+	if r.Method == http.MethodPost {
+		// Parse the form data
+		if err := r.ParseForm(); err != nil {
+			http.Error(w, "Error parsing form data", http.StatusBadRequest)
+			return
+		}
+
+		// Get the title and description from the form
+		title := r.Form.Get("title")
+		description := r.Form.Get("description")
+
+		// Create a new Todo
+		newTodo := Todo{
+			Title:       title,
+			Description: description,
+		}
+
+		// Add the newTodo to the todos list
+		todos = append(todos, newTodo)
+
+		// Respond with a success message
+		fmt.Fprintf(w, "Todo added successfully\n")
+
+		// Print the form data
+		fmt.Printf("Title: %s, Description: %s\n", title, description)
+		return
+	}
+
+	// Respond with the current list of todos for GET requests
+	if err := json.NewEncoder(w).Encode(todos); err != nil {
+		http.Error(w, "Error encoding todos", http.StatusInternalServerError)
+		return
+	}
 }
